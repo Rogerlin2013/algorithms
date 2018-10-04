56393c865cd8d9ad9a87620e42b6aa562aa4f19c
linyongzhi <rogerlin2010@gmail.com>
2018-10-04 15:02:17 +0800

    Initial Commit

---
+++ Algorithm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Algorithm
+//
+//  Created by linyongzhi on 2018/10/4.
+//  Copyright © 2018年 Beenlong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
