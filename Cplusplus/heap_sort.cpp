66bcc1fc8ae84b2742a538d94bce1231b045f3a8
linyongzhi <yongzhi.lin@uama.com.cn>
2018-11-19 13:50:19 +0800

    配置好了MacOS环境的C++编译和调试任务

---
+++ Cplusplus/heap_sort.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "max_heapify start..." << std::endl;
+    return 0;
+}
+
+
+void max_heapify(std::vector<int> &array, int i) {
+
+}
