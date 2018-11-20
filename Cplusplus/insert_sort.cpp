afce7ae7f5873fa69b346690857deddb75d88d10
linyongzhi <yongzhi.lin@uama.com.cn>
2018-11-20 17:02:10 +0800

    实现 insert_sort

---
+++ Cplusplus/insert_sort.cpp
@@ -0,0 +1,54 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+void insert_sort(vector<int> &array) {
+    int sorted_index = 0;
+
+    for (int i = 0; i < array.size(); ++i) {
+        
+        int newValue = array[i];
+        
+        int search_index = sorted_index;
+        int dest_pos = sorted_index;
+        while (search_index >= 0 && array[search_index] >= newValue) {
+            dest_pos = search_index;
+            --search_index;
+        }
+
+        if (dest_pos < sorted_index) {
+            // insert
+            int j = i;
+            while (j > dest_pos) {
+                array[j] = array[j - 1];
+                --j;
+            }
+            array[dest_pos] = newValue;
+        }
+
+        ++sorted_index;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    std::vector<int> array;
+    for (int i = 0; i < 30; ++i) {
+        array.push_back(random() % 100);
+    }
+
+    std::cout << "Array before start..." << std::endl;
+    std::vector<int>::iterator it;
+    for (it = array.begin(); it < array.end(); it++)
+        std::cout << ' ' << *it;
+    std::cout << '\n';
+
+    insert_sort(array);
+
+    std::cout << "Array after start..." << std::endl;
+    for (it = array.begin(); it < array.end(); it++)
+        std::cout << ' ' << *it;
+    std::cout << '\n';
+    return 0;
+}
