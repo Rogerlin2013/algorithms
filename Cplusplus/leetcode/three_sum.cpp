743f0be59bfc4b693df4e2a7eb8fe8b07bcd6ee4
roger <lin.yongzhi.roger@vertiv.com>
2019-12-17 10:38:49 +0800

    完善three_sum题解

--- Cplusplus/leetcode/three_sum.cpp
+++ Cplusplus/leetcode/three_sum.cpp
@@ -5,28 +5,36 @@
 using namespace std;
 
 vector<vector<int>> threeSum(vector<int>& nums) {
-    set<vector<int>> result;
+    vector<vector<int>> result;
 
     if (nums.size() < 3)
     {
-        return vector<vector<int>> (result.begin(), result.end());
+        return result;
     }
 
     std::sort(nums.begin(), nums.end());
     for (size_t i = 0; i < nums.size(); i++)
     {
+        if (i > 0 && nums[i] == nums[i - 1]) {
+            continue;
+        }
         for (size_t j = i + 1; j < nums.size(); j++)
         {
+            if (j > i + 1 && nums[j] == nums[j - 1]) {
+                continue;
+            }
             int target = -(nums[i] + nums[j]);
             int index = binary_search(nums, nums.size(), target);
             if (index != -1 && index > j)
             {
                 vector<int> haha = {nums[i], nums[j], nums[index]};
-                result.insert(haha);
+                result.push_back(haha);
             }
+            
         }
+        
     }
-    return vector<vector<int>> (result.begin(), result.end());
+    return result;
 }
 
 int binary_search(vector<int>& array, int n, int value) {
@@ -51,4 +59,54 @@ int binary_search(vector<int>& array, int n, int value) {
     }
 
     return -1;
+}
+
+vector<vector<int>> threeSum2(vector<int>& nums) {
+    vector<vector<int>> result;
+
+    if (nums.size() < 3)
+    {
+        return result;
+    }
+
+    std::sort(nums.begin(), nums.end());
+
+    for (size_t i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] > 0)
+        {
+            return result;
+        }
+        if (i > 0 && nums[i] == nums[i - 1]) {
+            continue;
+        }
+
+        int L = i + 1;
+        int R = nums.size() - 1;
+
+        while (L < R)
+        {
+            if (nums[i] + nums[L] + nums[R] == 0)
+            {
+                vector<int> haha = {nums[i], nums[L], nums[R]};
+                result.push_back(haha);
+                while (L < R && nums[L] == nums[L -1])
+                {
+                    L++;
+                }
+                
+                while (L < R && nums[R] == nums[R + 1]){
+                    R--;
+                }
+                L++;
+                R--;
+            }else if (nums[i] + nums[L] + nums[R] > 0)
+            {
+                R--;
+            }else {
+                L++;
+            }
+        }
+    }
+    return result;
 }
